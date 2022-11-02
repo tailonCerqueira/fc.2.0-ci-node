ba66ca7849e7987dd41ace7d200ee172c220ac08
Tailon Carvalho de Cerqueira <tailoncerqueira7@gmail.com>
2022-11-02 18:51:07 -0300

    new feat

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const techsService = require("./techs.service");
+const techsService = require("./src/techs.service");
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
   const techs = techsService.listOfTechs();
+  res.send(techs);
+});
+
+app.post("/", (req, res) => {
+  // const tec = req.body;
+  const tec = req.query.tecs;
+  const techs = techsService.pushTechs(tec);
   res.send(techs);
 });
 
