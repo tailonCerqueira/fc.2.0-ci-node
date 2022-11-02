47daf2002539a8aa16f3f5d8355ffc8ed2dd51e3
Tailon Carvalho de Cerqueira <tailoncerqueira7@gmail.com>
2022-11-02 14:04:44 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("OK");
+});
+
+app.listen(3000, () => {
+  console.log("Starting application");
+});
